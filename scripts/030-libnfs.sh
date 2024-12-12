49d6f8e67ca35b204ddbd647b0bd5ced1833b760
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2024-12-12 13:49:36 +0800

    更新 030-libnfs.sh

--- scripts/030-libnfs.sh
+++ scripts/030-libnfs.sh
@@ -4,10 +4,10 @@
 #	ported to PS3 by Bucanero
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/sahlberg/libnfs/tarball/master -O libnfs.tar.gz 
+wget --no-check-certificate https://github.com/sahlberg/libnfs/archive/3fd5d8809456503c11364bc49aba9ba9aa90a1af.zip -O libnfs.zip
 
 ## Unpack the source code.
-rm -Rf libnfs && mkdir libnfs && tar --strip-components=1 --directory=libnfs -xvzf libnfs.tar.gz && cd libnfs
+rm -Rf libnfs && mkdir libnfs && unzip libnfs.zip && mv libnfs-3fd5d8809456503c11364bc49aba9ba9aa90a1af libnfs && cd libnfs
 
 ## Compile and install.
-${MAKE:-make} -f ps3_ppu/Makefile.PS3_PPU install
+${MAKE:-make} V=1 VERBOSE=1 -f ps3_ppu/Makefile.PS3_PPU install
